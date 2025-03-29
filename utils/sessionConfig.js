5d478eb58fdfca039972f872df83909a97683f33
Ruan Patrick <114404700+ruan177@users.noreply.github.com>
2025-03-28 21:10:21 -0300

    Update sessionConfig.js

--- utils/sessionConfig.js
+++ utils/sessionConfig.js
@@ -1,24 +1,7 @@
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const { Session } = require('../models/Session');
-const { Sequelize } = require('sequelize');
-
-const sequelizeSession = new Sequelize({
-    username: 'postgres',
-    password: 'residentevil6',
-    database: 'ifvest2',
-    host: 'localhost',
-    dialect: 'postgres',
-    port: 5432,
-});
 
 const sessionOptions = {
     secret: 'frasealeatoria',
-    store: new SequelizeStore({ 
-        db: sequelizeSession , 
-        tableName: "sessions",
-        model: Session,
-      }),
     resave: false,
     saveUninitialized: false,
     cookie: { 
